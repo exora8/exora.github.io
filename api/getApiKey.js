1b2354b7b708910f571f31c25f4d398e56c8dd99
exora8 <exorafuturesai@gmail.com>
2025-05-20 09:34:00 +0700

    Delete api directory

--- api/getApiKey.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-    const apiKeys = process.env.API_KEYS.split(","); // Ambil dari env & pisah jadi array
-    const randomKey = apiKeys[Math.floor(Math.random() * apiKeys.length)]; // Pilih random
-
-    res.status(200).json({ apiKey: randomKey });
-}
